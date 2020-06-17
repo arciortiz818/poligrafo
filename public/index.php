5b6c2840b5d53bbfd3fde55de244483ad3dbc958
Javier Arciniegas <arciniegas.ortiz818@gmail.com>
2020-06-17 13:01:22 -0500

    Cors

--- public/index.php
+++ public/index.php
@@ -13,6 +13,11 @@
 // header('Access-Control-Max-Age: 1000');
 // header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
+header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+        
 define('LARAVEL_START', microtime(true));
 
 /*
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,12 +24,7 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <?php
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-        ?>
+ 
     </head>
     <body>
         @yield('content')
